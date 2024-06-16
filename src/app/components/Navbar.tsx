6c4a9cf48ceb74986f669a920cb3666bf6695c81
kasun Aththanagoda <kasun@icanlk.com>
2024-06-16 14:07:07 +0530

    mobile nav bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Menu from "./Menu";
 
 export default function Navbar() {
   return (
-    <div>
-      
+    <div className="h-20 px-4 md:px-8 lg:px-16 2xl:px-64 relative">
+      {/* Mobile */}
+      <div className="flex items-center justify-between h-full">
+        <Link href="/">
+          <div className="text-2xl tracking-wide">E-shop</div>
+        </Link>
+        <Menu></Menu>
+      </div>
     </div>
-  )
+  );
 }
