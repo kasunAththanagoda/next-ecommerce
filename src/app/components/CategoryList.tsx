c754358f91f028b585fb1bc608585c34691a4cb1
kasun Aththanagoda <kasun@icanlk.com>
2024-06-27 00:59:56 +0530

    category list

---
+++ src/app/components/CategoryList.tsx
@@ -0,0 +1,112 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function CategoryList() {
+  return (
+    <div className='px-4 overflow-x-scroll scrollbar-hide'>
+        <div className='flex gap-4 md:gap-8'>
+            <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+
+                <Link href="/list>?cat=text" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+                <Image 
+          src="https://images.pexels.com/photos/25935100/pexels-photo-25935100/free-photo-of-work-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                
+                alt="" fill sizes="5vw" className=""></Image>                
+                </div>
+                <h1 className='mt-8 font-light text-xl tracking-wide'>Category name</h1>
+                </Link>
+        </div>
+      
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Slider from "@/app/components/Slider"
 import ProductList from "./components/ProductList"
+import CategoryList from "./components/CategoryList"
 
 const HomePage = () => {
   return (
@@ -7,6 +8,17 @@ const HomePage = () => {
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
+
+      <ProductList />
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl mb-8 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">Categories</h1>
+
+      <CategoryList />
+      </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
 
       <ProductList />
       </div>
