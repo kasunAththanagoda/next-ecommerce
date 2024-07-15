fa51bed97b9654c58e4402fda0aeef605a744bfa
kasun Aththanagoda <kasun@icanlk.com>
2024-07-15 22:56:08 +0530

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Test from "./components/Test";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +22,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar></Navbar>
         {children}
-        <Footer>
-          </Footer></body>
+        <Test />
+        <Footer />
+          </body>
     </html>
   );
 }
