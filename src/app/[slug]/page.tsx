c6c894e1ca5a5a0841cc1a6172837858f0751bdb
kasun Aththanagoda <kasunaththanagoda@gmail.com>
2024-07-30 00:53:57 +0530

    choose color

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import ProductImages from '../components/ProductImages'
+import CustomizeProducts from '../components/CustomizeProducts'
+import Add from '../components/Add'
 
 export default function SinglePage() {
   return (
     <div className='px-4 md:px-8 lg:px-16 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
       {/* img  */}
-      <div className='w-full lg:w-1/2'>
+      <div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
         <ProductImages />
       </div>
       {/* text  */}
-      <div className=''>
-        description
+      <div className='w-full lg:w-1/2 flex flex-col gap-6'>
+      <h1 className="text-4xl font-medium">Product Details</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos atque quibusdam magni optio quia suscipit exercitationem, impedit error rerum quis praesentium temporibus minima, maxime ipsum mollitia, repellat incidunt autem magnam.</p>
+      
+      <div className='h-[2px] bg-gray-100'></div>
+      <div className='flex items-center gap-4'>
+        <h3 className='text-xl text-gray-500 line-through'>50</h3>
+        <h3 className='font-medium text-2xl'>60</h3>
+      </div>
+      <div className='h-[2px] bg-gray-100'></div>
+      <div className='text-sm'>
+        <h4 className='font-medium mb-4'>Title</h4>
+        <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nam veritatis tempore rerum autem nostrum in aliquam vitae libero, quis iusto delectus sapiente corrupti amet laudantium dignissimos ex officia possimus!</p>
+      </div><div className='text-sm'>
+        <h4 className='font-medium mb-4'>Title</h4>
+        <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nam veritatis tempore rerum autem nostrum in aliquam vitae libero, quis iusto delectus sapiente corrupti amet laudantium dignissimos ex officia possimus!</p>
+      </div><div className='text-sm'>
+        <h4 className='font-medium mb-4'>Title</h4>
+        <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nam veritatis tempore rerum autem nostrum in aliquam vitae libero, quis iusto delectus sapiente corrupti amet laudantium dignissimos ex officia possimus!</p>
+      </div>
+
+      <CustomizeProducts />
+      <Add />
       </div>
 
     </div>
---
+++ src/app/components/CustomizeProducts.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function CustomizeProducts() {
+  return (
+    <div className="flex flex-col gap-6">
+      <h4 className='font-medium'>Choose a color</h4>
+      <ul className='flex items-center gap-3'>
+      <li className='w-8 h-8 bg-red-500 rounded-full ring-1 ring-gray-300 cursor-pointer relative'>
+            <div className=' absolute h-10 w-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
+        </li>
+        <li className='w-8 h-8 bg-blue-500 rounded-full ring-1 ring-gray-300 cursor-pointer relative'>
+        </li>
+        <li className='w-8 h-8 bg-green-500 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative'>
+        <div className='absolute bg-red-400 top-1/2 left-1/2 w-10 h-[2px] rotate-45 transform -translate-x-1/2 -translate-y-1/2'></div>
+        </li>
+      </ul>
+
+
+
+      <h4 className='font-medium'>Choose a size</h4>
+      <div className='flex items-center gap-3'></div>
+    </div>
+  )
+}
