293ac04207ae565d4d8812bad8566095d85ed7ca
kasun Aththanagoda <kasunaththanagoda@gmail.com>
2024-07-29 23:47:40 +0530

    product image page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ProductImages from '../components/ProductImages'
 
 export default function SinglePage() {
   return (
-    <div>
-      
+    <div className='px-4 md:px-8 lg:px-16 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
+      {/* img  */}
+      <div className='w-full lg:w-1/2'>
+        <ProductImages />
+      </div>
+      {/* text  */}
+      <div className=''>
+        description
+      </div>
+
     </div>
   )
 }
